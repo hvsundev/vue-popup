40872f0cdf8e0c32366e8ad9cc6782d50c3ccf53
hvsundev <hvsundev@gmail.com>
2021-06-15 23:15:02 +0900

    slide 추가

--- src/page/popup/SlideView.vue
+++ src/page/popup/SlideView.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-      slide-view
+    <vueper-slides class="no-shadow" fixed-height="400px">
+      <vueper-slide title="01"></vueper-slide>
+      <vueper-slide title="02"></vueper-slide>
+      <vueper-slide title="03"></vueper-slide>
+    </vueper-slides>
   </div>
 </template>
 
 <script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
 export default {
+
+  components: {
+    VueperSlides, VueperSlide
+  }
 
 }
 </script>
